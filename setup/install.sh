d6f20e93c027b7ff883426285180aed02332406e
idovitz <idovitz@96dd3d3a-e750-0410-a8df-79adf40cd92f>
2008-12-12 13:46:23 +0000

    - setup files

---
+++ setup/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+CONFIGDIR=/etc/couvstream
+
+if test `whoami` = "root"
+then
+	echo -n "Do you realy want to install couvstream, this will overide all configuration!!!!
+[ja/nee]: "
+	read INSTALL
+
+	if [ "$INSTALL" = "ja" ]
+	then
+		PORTB=8000
+		echo -en "\nBegin port range for stream processes [8000]: "
+		read PORTB_R
+		if [ "$PORTB_R" != "" ]
+		then
+			PORTB=$PORTB_R
+		fi
+
+		while [ "$NRCAMS" = "" ]
+		do 
+			echo -en "\nNumber of camera's: "
+			read NRCAMS
+		done
+
+		######### copy config
+		if test -d
+		then
+			mkdir -p $CONFIGDIR/cams
+			cp ../config/example-config.xml $CONFIGDIR/config.xml
+		fi
+		
+
+		######### make apache config files
+		for i in `seq 1 $NRCAMS`
+		do
+			./addcam $i $PORTB > $CONFIGDIR/cams/cam$i
+		done
+	fi
+else
+	echo "run as root!"
+fi
---
+++ setup/linking.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm /usr/bin/strcond.py
+rm /usr/bin/ircutd.py
+rm /usr/bin/selfmon.py
+rm /usr/bin/streamchecker.py
+rm /usr/bin/streamcounter.py
+rm /usr/bin/filterchecker.py
+
+find ../streamcontrol/ -name "*.py" -type f -exec chmod 775 {} \;
+
+ln -s `pwd`/../streamcontrol/strcond.py /usr/bin/
+ln -s `pwd`/../streamcontrol/ircutd.py /usr/bin/
+ln -s `pwd`/../streamcontrol/selfmon.py /usr/bin/
+ln -s `pwd`/../streamcontrol/streamchecker.py /usr/bin/
+ln -s `pwd`/../streamcontrol/streamcounter.py /usr/bin/
+ln -s `pwd`/../streamcontrol/filterchecker.py /usr/bin/
