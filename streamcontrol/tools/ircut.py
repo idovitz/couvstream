c3bd424439cf47d002cade93f351750a21342148
idovitz <idovitz@96dd3d3a-e750-0410-a8df-79adf40cd92f>
2009-07-21 13:39:09 +0000

    - missing file

---
+++ streamcontrol/tools/ircut.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+##################################################
+# projectname: couvstream
+# Copyright (C) 2007  IJSSELLAND ZIEKENHUIS
+###################################################
+
+import elementtree.ElementTree as ET
+import os, string, sys, smtplib, time, urllib, socket
+from SOAPpy import SOAPProxy, Error
+
+apath = os.getcwd().replace("/tools", "")
+sys.path.append(apath)
+from utils import *
+
+options = OptionParser(sys.argv)
+print options.c
+config = ConfigLoader(options.c)
+l = Log(config.loglevel, "ircut")
+
+url = "http://localhost:%s" % config.listenport
+namespace = "urn:couvstream"
+
+# timeout in seconds
+timeout = 50
+socket.setdefaulttimeout(timeout)
+
+def switchIrcut(n):
+    try:
+        client = SOAPProxy(url, namespace=namespace)
+        report = client.switchIRFilter("all", n)
+        msg = "Subject: [FAMNET] IR CUT FILTER report\n\nreport:\n%s" % report
+    except Error, s:
+        msg="Subject: [FAMNET] connection to streamer failed\n%s" % s
+
+    server = smtplib.SMTP("localhost")
+    server.sendmail(config.fromaddr, config.toaddr, msg)
+    server.quit()
+    
+print "switch ircut %s" % options.s
+switchIrcut(options.s)
